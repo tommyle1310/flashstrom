c255a352a7b13ff42de600dfdb35a14119288985
tommyle1310 <tommyle1310@gmail.com>
2025-02-08 10:32:27 +0700

    upload avatar

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';  // Import the ValidationPipe
+import { ValidationPipe } from '@nestjs/common'; // Import the ValidationPipe
 import * as dotenv from 'dotenv';
-import { HttpExceptionFilter } from './utils/createResponse';  // Your custom exception filter
+import { HttpExceptionFilter } from './utils/createResponse'; // Your custom exception filter
 
 dotenv.config();
 
@@ -10,19 +10,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || '*',  // You can specify allowed origins here, for example, 'http://localhost:3000'
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Allowed HTTP methods
-    allowedHeaders: 'Content-Type, Accept',  // Allowed headers
-    credentials: true,  // Allow credentials (cookies, authorization headers)
+    origin: process.env.CORS_ORIGIN || '*', // You can specify allowed origins here, for example, 'http://localhost:3000'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers)
   });
-
 
   // Use the ValidationPipe globally with the whitelist option
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,  // Automatically remove properties that do not exist in the DTO
-      forbidNonWhitelisted: true,  // Throw an error if there are non-whitelisted properties
-      transform: true,  // Automatically transform payloads to the DTO class type
+      whitelist: true, // Automatically remove properties that do not exist in the DTO
+      forbidNonWhitelisted: true, // Throw an error if there are non-whitelisted properties
+      transform: true, // Automatically transform payloads to the DTO class type
     }),
   );
 
