c25d87324fc8a4cfdb96ae70570db647321fc384
tommyle1310 <tommyle1310@gmail.com>
2025-03-30 20:25:29 +0700

    online session group by date

--- src/drivers/drivers.controller.ts
+++ src/drivers/drivers.controller.ts
@@ -11,10 +11,15 @@ import {
 import { DriversService } from './drivers.service'; // Corrected to use DriversService
 import { CreateDriverDto, UpdateVehicleDto } from './dto/create-driver.dto'; // Corrected to use CreateDriverDto
 import { UpdateDriverDto } from './dto/update-driver.dto'; // Corrected to use UpdateDriverDto
+import { OnlineSessionsService } from 'src/online-sessions/online-sessions.service';
+import { createResponse } from 'src/utils/createResponse';
 
 @Controller('drivers') // Updated route to 'drivers'
 export class DriversController {
-  constructor(private readonly driversService: DriversService) {} // Corrected service to DriversService
+  constructor(
+    private readonly driversService: DriversService,
+    private readonly onlineSessionsService: OnlineSessionsService
+  ) {} // Corrected service to DriversService
 
   @Post()
   create(@Body() createDriverDto: CreateDriverDto) {
@@ -24,6 +29,59 @@ export class DriversController {
   @Get()
   findAll() {
     return this.driversService.findAll(); // Corrected service method to use driversService
+  }
+
+  @Get('/online-session/:driver')
+  async findOnlineSessionByDriverId(
+    @Param('driver') driverId: string,
+    @Query('limit') limit?: string,
+    @Query('offset') offset?: string
+  ) {
+    const limitNum = limit ? parseInt(limit, 10) : 10; // Mặc định 10
+    const offsetNum = offset ? parseInt(offset, 10) : 0; // Mặc định 0
+    const response = await this.onlineSessionsService.findByDriverId({
+      driverId,
+      limit: limitNum,
+      offset: offsetNum
+    });
+    const { EC, EM, data } = response;
+    if (EC === 0) {
+      const groupedData = data.reduce(
+        (acc: any[], curr: { start_time: number; end_time: number }) => {
+          // Chuyển timestamp sang định dạng ngày
+          const date = new Date(parseInt(`${curr.start_time}`) * 1000)
+            .toISOString()
+            .split('T')[0];
+
+          // Tính thời gian online của object hiện tại (milliseconds)
+          const onlineTime =
+            (parseInt(`${curr.end_time}`) - parseInt(`${curr.start_time}`)) *
+            1000;
+
+          // Tìm nhóm theo ngày
+          let group = acc.find(item => item.date === date);
+          if (!group) {
+            group = { date: date, items: [], total_online_hours: 0 };
+            acc.push(group);
+          }
+
+          // Thêm object vào nhóm và cộng dồn thời gian
+          group.items.push(curr);
+          group.total_online_hours += onlineTime;
+
+          return acc;
+        },
+        []
+      );
+      const result = groupedData.map(group => ({
+        date: group.date,
+        items: group.items,
+        total_online_hours: this.driversService.formatTime(
+          group.total_online_hours
+        )
+      }));
+      return createResponse('OK', result, null);
+    }
   }
 
   @Get('/driver-progress-stages/:driver')
--- src/drivers/drivers.service.ts
+++ src/drivers/drivers.service.ts
@@ -587,4 +587,12 @@ export class DriversService {
       return this.handleError('Error fetching dps:', error);
     }
   }
+
+  async formatTime(milliseconds) {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
 }
