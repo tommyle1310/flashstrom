d91327faa8e1cfff1f672274f9528fe95ee9fd1d
Hoangnguyen2610 <V.C.L.32141@gmail.com>
2025-05-12 10:10:30 +0700

    fix customer care module

--- src/customer_cares/customer_cares.repository.ts
+++ src/customer_cares/customer_cares.repository.ts
@@ -102,11 +102,7 @@ export class CustomerCaresRepository {
     return await this.repository.findAndCount({
       skip,
       take: limit,
-<<<<<<< HEAD
       relations: ['user_id']
-=======
-      relations: ['user']
->>>>>>> d472b5f2d4a15452368a4da85205d6839bcd1d02
     });
   }
 }
