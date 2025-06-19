ff1580d81d8f6a4c892a988c6fc406fd42d2d86f
tommyle1310 <tommyle1310@gmail.com>
2025-06-19 18:54:06 +0700

    implement .rating_stats to retrieving restaurant details api, add sample images in image_urls.ts

--- src/assets/image_urls.ts
+++ src/assets/image_urls.ts
@@ -2,5 +2,16 @@ export const IMAGE_LINKS = {
   LIGHT_FLASHFOOD_LOGO:
     'https://res.cloudinary.com/dpubnzap3/image/upload/v1738818798/ei6ycutzsexeu90e8bxn.png',
   ORDER_DELIVERED:
-    'https://res.cloudinary.com/dlavqnrlx/image/upload/v1744247867/success_jwwzov.png'
+    'https://res.cloudinary.com/dlavqnrlx/image/upload/v1744247867/success_jwwzov.png',
+  // Menu item avatars
+  MENU_1:
+    'https://res.cloudinary.com/dlavqnrlx/image/upload/v1750306648/pugfy7a6bkadjrwseaib.png',
+  MENU_2:
+    'https://res.cloudinary.com/dlavqnrlx/image/upload/v1750306648/jevvgx3gpjkscz6poxou.png',
+  MENU_3:
+    'https://res.cloudinary.com/dlavqnrlx/image/upload/v1750306647/e1nb5selaxmy9jjyiv48.png',
+  MENU_4:
+    'https://res.cloudinary.com/dlavqnrlx/image/upload/v1750306647/murazdkdl711ma0xg0je.png',
+  MENU_5:
+    'https://res.cloudinary.com/dlavqnrlx/image/upload/v1747281638/mgdczz0mk7ogoborzgn6.jpg'
 };
